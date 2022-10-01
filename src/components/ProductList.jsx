6819f48c7a82d2166ed2a464b4ec373047831b70
Reena Soni <reenaprahladsoni@gmail.com>
2022-10-01 20:33:23 +0530

    implemented pagination using MUI and react hooks

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,30 +1,64 @@
-import { Container, Typography } from "@mui/material";
-import React from "react";
+import { Container, Grid, Pagination, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../features/productSlice";
+import usePagination from "./Pagination";
+import Products from "./Products";
 
 const ProductList = () => {
   const products = useSelector((state) => state.products);
+  console.log(products);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const count = Math.ceil(products.products.length / itemsPerPage);
+  const currentItems = usePagination(products.products, itemsPerPage);
+  console.log(currentItems);
 
   const dispatch = useDispatch();
-  React.useEffect(() => {
+  useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  const handleChange = (e, p) => {
+    setCurrentPage(p);
+    currentItems.jump(p);
+  };
+
   return (
-    <Container sx={{ bgcolor: "red", height: "fit-content" }}>
+    <Container
+      sx={{
+        height: "fit-content",
+      }}
+    >
       <Typography variant="h3">Our Products</Typography>
       {products.loading && <Typography>Loading....</Typography>}
       {!products.loading && products.error ? (
         <Typography>Opps! there is some error</Typography>
       ) : null}
       {!products.loading && products.products.length ? (
-        <ul>
-          {products.products.map((prod) => (
-            <li key={prod.id}>{prod.title}</li>
+        <Grid
+          container
+          justifyContent="space-around"
+          display={"flex"}
+          flexWrap="wrap"
+        >
+          {currentItems.currentData().map((prod) => (
+            <Grid item key={prod.id}>
+              <Products product={prod} />
+            </Grid>
           ))}
-        </ul>
+        </Grid>
       ) : null}
+      <Pagination
+        count={count}
+        size="large"
+        page={currentPage}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+      />
     </Container>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,1 +1,77 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { ShoppingCart } from "@mui/icons-material";
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Products({ product }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 245 }}>
+      <CardHeader
+        variant="h6"
+        title={product.title}
+        subheader={product.category}
+      />
+      <CardMedia
+        component="img"
+        height="fit-content"
+        image={product.image}
+        alt="product images"
+      />
+      <CardContent>
+        <Typography color="text.secondary">Price : ${product.price}</Typography>
+        <Typography color="text.secondary">
+          Rating : {product.rating.rate}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <ShoppingCart />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {product.description}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
